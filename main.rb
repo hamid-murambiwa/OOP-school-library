cebf633d414eff883d13c92131eb04436b1db102
hamid-murambiwa <machipisajunior@gmail.com>
2022-01-29 09:21:21 +0200

    Fix linters errors

--- main.rb
+++ main.rb
@@ -164,22 +164,22 @@ class App
   def create_rental
     puts 'Select one of the books of the list below: '
     @books.each_with_index { |book, index| puts "#{index}) Title: #{book.title}, Author: #{book.author}" }
-  
+
     book_id = gets.chomp.to_i
-  
+
     puts 'Select a person number (not id) from the list below'
     @people.each_with_index do |person, index|
       puts "#{index}) [#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
     end
-  
+
     person_id = gets.chomp.to_i
-  
+
     print 'Date: '
     date = gets.chomp.to_s
-  
+
     rental = Rental.new(date, @people[person_id], @books[book_id])
     @rentals << rental
-  
+
     puts 'Rental created'
     sleep 1.2
   end
