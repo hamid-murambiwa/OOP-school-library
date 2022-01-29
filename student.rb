69d612820abae723830f5570f3dd44fe01c3c7c2
hamid-murambiwa <machipisajunior@gmail.com>
2022-01-29 07:15:59 +0200

    Made the following changes: Add main.rb file Add List all books method Add all_people method Add create_person method Add create_student method Add create_teacher method Add create_book method Add create_rental method Add all_rental_by_id method

--- student.rb
+++ student.rb
@@ -3,7 +3,7 @@ require_relative 'classroom'
 
 class Student < Person
   def initialize(classroom, age, name = 'Unknown', parent_permission: true)
-    super(age, name, parent_permission)
+    super(age, name, parent_permission: parent_permission)
     @classroom = classroom
   end
 
