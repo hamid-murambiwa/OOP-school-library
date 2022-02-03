36c296ab095baaabcf96eb9c4f7dbfadb994529f
hamid-murambiwa <machipisajunior@gmail.com>
2022-02-03 12:41:42 +0200

    Add List module to the query_data.rb file

---
+++ query_data.rb
@@ -0,0 +1,32 @@
+module List
+  def all_books
+    @books.each do |book|
+      puts "Title: \"#{book.title}\", Author: #{book.author}"
+    end
+    puts "\n"
+    resume
+  end
+
+  def all_people
+    @people.each do |person|
+      puts "[#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
+    end
+    puts "\n"
+    resume
+  end
+
+  def all_rental_by_id
+    print 'ID of person: '
+    user_id = gets.chomp
+
+    puts 'Rentals:'
+    @rentals.each do |rental|
+      if rental.person.id.to_s == user_id
+        puts "Date: #{rental.date}, Book \"#{rental.book.title}\" by #{rental.book.author}"
+      end
+    end
+
+    puts "\n"
+    resume
+  end
+end
